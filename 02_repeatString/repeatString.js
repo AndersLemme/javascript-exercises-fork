70b46728fcb47391c4282860f2f0fe150072b956
AndersLemme <a.lemme@hotmail.com>
2025-02-24 19:32:46 +0100

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(x, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    let i = 0;
+    let hey = "";
+    while( i < num){
+        hey += x;
+        i++;
+    }
+    return hey
 };
 
 // Do not edit below this line
