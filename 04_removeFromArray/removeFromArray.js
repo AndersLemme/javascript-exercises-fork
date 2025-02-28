3cfd961104511b6c56cd498c6be020c5557feb49
AndersLemme <a.lemme@hotmail.com>
2025-02-28 18:50:27 +0100

    Completed excersice 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
-const removeFromArray = function(arr, num) {
-    arr.splice(arr.indexOf(num), 1)
+const removeFromArray = function(arr, ...values) {
+    //using filter instead of splice to remove multiple of the same number
+    arr = arr.filter(item => !values.includes(item)); 
+    
+
+    /*
+    for (let each of values) {
+        arr.splice(arr.indexOf(each), 1);
+        console.log(each)
+    }*/
+
     return arr
 };
-
-// Do not edit below this line
+    
 module.exports = removeFromArray;
