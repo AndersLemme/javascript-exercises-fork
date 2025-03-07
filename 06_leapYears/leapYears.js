f4d8be98b8c85d32440f6867b27a6d74795666bd
AndersLemme <a.lemme@hotmail.com>
2025-03-07 21:30:50 +0100

    Completed excersice 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(year%4 == 0){
+        if(year%400 == 0){
+            return true;
+        }else if(year%100 ==0){
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//=IF(OR(MOD(A1,400)=0,AND(MOD(A1,4)=0,MOD(A1,100)<>0)),"Leap Year", "NOT a Leap Year")  
