eb53788ad7b871aa88e0da495f2682c63fe405ac
AndersLemme <a.lemme@hotmail.com>
2025-03-10 22:35:31 +0100

    Completed task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let i=0; i<arr.length; i++){
+    sum =sum + arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  for(let i=0; i<arr.length; i++){
+    mul =mul * arr[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let sum = 1;
+  for(let i = 1; i<n+1; i++){
+    sum = sum*i
+  }
+  return sum
 };
 
 // Do not edit below this line
