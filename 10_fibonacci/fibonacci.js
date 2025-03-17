51beabc879879dc3814d5d510dc56df14b3e6ba5
AndersLemme <a.lemme@hotmail.com>
2025-03-17 22:57:02 +0100

    Coded fibonacci. Need to add 0 at fib num 0. And I need to make it accept string

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,13 @@
-const fibonacci = function(n) {
+const fibonacci = function(num) {
+    
+    let n=[1,1];
+    
     let ans = 0;
-    for(let i = 1; i<n; i++){
-        ans += i-1 //recursive?
-        
+    for(let i = 2; i<num; i++){
+        n[i] = n[i-1] + n[i-2];
+        //console.log(n);
     }
+    return n[n.length-1];
 
 };
 
