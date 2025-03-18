1b22a6e53a1f60b6f238ed2b16b215a9a03b03ee
AndersLemme <a.lemme@hotmail.com>
2025-03-18 08:22:38 +0100

    Completed task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,16 @@
 const fibonacci = function(num) {
-    
-    let n=[1,1];
+    num =Number(num);
+    if (num < 0){
+        return "OOPS";
+    }
+    let n=[0,1];
     
     let ans = 0;
-    for(let i = 2; i<num; i++){
+    for(let i = 2; i<num+1; i++){
         n[i] = n[i-1] + n[i-2];
         //console.log(n);
     }
-    return n[n.length-1];
+    return n[num];
 
 };
 
