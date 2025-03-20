ae5dc113f8e83d7d7e654a71be3c3b470fc3b962
AndersLemme <a.lemme@hotmail.com>
2025-03-20 22:23:44 +0100

    Completed task 11, two different solutions :)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles1 = function(obj) {
+let ans = [];
+    for(let i = 0; i< obj.length; i++){
+        ans.push(obj[i].title);
+    }
+    return ans;
 };
-
+const getTheTitles = function(obj) {
+    return obj.map(book=>book.title)
+}
 // Do not edit below this line
 module.exports = getTheTitles;
